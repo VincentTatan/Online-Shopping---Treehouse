b8758187b757012fe07caddd0041198357d41fbd
Vincent Tatan <vincentt.2013@sis.smu.edu.sg>
2016-01-06 09:55:57 +0800

    Code controllers to detect pagination and return shirts in subsets

--- shirts/shirts.php
+++ shirts/shirts.php
@@ -7,8 +7,20 @@ if (empty($_GET["pg"])){
 } else{
 	$current_page = $_GET["pg"];
 }
-$products = get_products_all();
+$current_page = intval($current_page);
 
+$total_products = get_products_count();
+$products_per_page = 8;
+$total_pages = ceil($total_products/$products_per_page);
+if($current_page>$total_pages){
+	header("Location: ./pg=".$total_pages);
+}
+
+if($current_page<1){
+	header("Location: ./");
+}
+
+$products = get_products_subset($start,$end);
 
 $pageTitle ="Shirts Catalogue";
 $section = "shirts";
